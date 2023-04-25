4a2e780424a12b98184dbd96c4fee5184cb2dd57
Paolo Conizzoli <paolo.conizzoli@gmail.com>
2023-04-25 19:14:41 +0200

    Created example file to display library usages

---
+++ examples/osv.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"osv"
+	osvClient "osv/http"
+)
+
+func main() {
+	pckInfo := osv.PackageInfo{Name: "org.apache.logging.log4j:log4j", Ecosystem: "Maven"}
+	vulnerabilityQuery := osv.VulnerabilityQuery{Version: "2.1", PckInfo: pckInfo}
+
+	osvClient := osvClient.OsvHttpClient{}
+	osvClient.OsvHttpClient(http.Client{})
+	myVulns, _ := osv.QueryVulnerabilities(&osvClient, &vulnerabilityQuery)
+
+	response, _ := osv.GetVulnerability(&osvClient, myVulns.Vulns[0].ID)
+	fmt.Println(response)
+
+	q1 := osv.VulnerabilityQuery{PckInfo: osv.PackageInfo{Purl: "pkg:pypi/antlr4-python3-runtime@4.7.2"}}
+	q2 := osv.VulnerabilityQuery{Commit: "6879efc2c1596d11a6a6ad296f80063b558d5e0f"}
+
+	queries := osv.VulnerabilityQueries{Queries: []osv.VulnerabilityQuery{q1, q2}}
+
+	response2, _ := osv.BulkQueryVulnerabilities(&osvClient, &queries)
+	fmt.Println(response2)
+}
